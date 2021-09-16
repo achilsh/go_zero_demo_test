ee46bcee0b7830127b5f12ce6179b07ec67fccca
Hong Hu <hong.hu@anker.com>
2021-09-16 17:38:52 +0800

    多数据库， 多模型 使用

--- micro_demo/message/internal/config/config.go
+++ micro_demo/message/internal/config/config.go
@@ -7,7 +7,7 @@ import (
 
 type Config struct {
 	zrpc.RpcServerConf
-	DataSource string
-	Table      string
-	Cache      cache.CacheConf
+	DataSource string          //db 配置,可以配置多个db数据源,名字和config 文件的key保持一致
+	Table      string          //某一个表名，当然可以配合多个表名， 名字和config文件的key保持一致; 实际查询不使用该配置。
+	Cache      cache.CacheConf //cache 配置
 }
--- micro_demo/message/internal/svc/servicecontext.go
+++ micro_demo/message/internal/svc/servicecontext.go
@@ -8,12 +8,14 @@ import (
 
 type ServiceContext struct {
 	Config config.Config
-	Mem    model.ShorturlModel //新增数据模型
+	Mem    model.ShorturlModel //新增数据模型1,
+	Other  model.BookModel     //新增数据模型2, 涉及到多个表
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
-		Mem:    model.NewShorturlModel(sqlx.NewMysql(c.DataSource), c.Cache), //根据数据库配置创建model
+		Mem:    model.NewShorturlModel(sqlx.NewMysql(c.DataSource), c.Cache), //根据数据库配置创建model和cache，此处cache和db关联起来
+		Other:  model.NewBookModel(sqlx.NewMysql(c.DataSource)),
 	}
 }
