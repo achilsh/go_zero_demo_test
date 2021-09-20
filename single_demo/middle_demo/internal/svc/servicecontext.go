b400a95747bb144ace64f83c5c94b5a00fbb8524
hwshtongxin <hwshtongxin@126.com>
2021-09-20 16:44:47 +0800

    路由中间件 的实践

--- single_demo/middle_demo/internal/svc/servicecontext.go
+++ single_demo/middle_demo/internal/svc/servicecontext.go
@@ -8,15 +8,17 @@ import (
 )
 
 type ServiceContext struct {
-	Config     config.Config
-	AuthMiddle rest.Middleware //需要手动定义
-	Middle2    rest.Middleware //需要手动定义
+	Config       config.Config
+	AuthMiddle   rest.Middleware //需要手动定义
+	Middle2      rest.Middleware //需要手动定义
+	AuthMiddle_2 rest.Middleware //需要手动定义
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config:     c,
-		AuthMiddle: middleware.NewAuthMiddleMiddleware(context.Background(), c.Name).Handle, //需要手动添加
-		Middle2:    middleware.NewAuthMiddle_2Middleware().Handle,                           //需要手动添加
+		Config:       c,
+		AuthMiddle:   middleware.NewAuthMiddleMiddleware(context.Background(), c.Name).Handle, //需要手动添加
+		Middle2:      middleware.NewAuthMiddle_2Middleware().Handle,                           //需要手动添加
+		AuthMiddle_2: middleware.NewAuthMiddle_2Middleware().Handle,                           //需要手动添加
 	}
 }
